a7e4667d07f197d2b881994b76a77145cd911b8a
Burak Kose <pro.burak.kose@gmail.com>
2025-10-25 15:59:15 +0200

    Fix env variable retrieval

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,2 @@
-import dotenv from 'dotenv'; 
-
-dotenv.config();
-
-export const apiUrl = process.env.BaseUrl;
-export const apiKey = process.env.OMDbApiKey;
+export const apiUrl = process.env.REACT_APP_BaseUrl;
+export const apiKey = process.env.REACT_APP_OMDbApiKey;
