85be78e030a222a66d133cdc52f7aec33d0b14bc
Burak Kose <pro.burak.kose@gmail.com>
2025-10-25 14:46:10 +0200

    Add movie, home, show template detail pages

---
+++ src/pages/MovieDetail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const MovieDetail = () => {
+  return (
+    <div>
+      <h1>Welcome to the Movie Detail Page</h1>
+      <p>This is the detail page for a specific movie.</p>
+    </div>
+  );
+};
+
+export default MovieDetail;
---
+++ src/pages/ShowDetail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const ShowDetail = () => {
+  return (
+    <div>
+      <h1>Welcome to the Show Detail Page</h1>
+      <p>This is the detail page for a specific show.</p>
+    </div>
+  );
+};
+
+export default ShowDetail;
