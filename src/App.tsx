27031833875354caf8d33a37dbbd42db216fe1a1
Burak Kose <pro.burak.kose@gmail.com>
2025-10-25 15:28:27 +0200

    Update main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-import MovieDetail from './pages/MovieDetail';
 import { Route, Routes } from 'react-router-dom';
-import ShowDetail from './pages/ShowDetail';
+import Details from './pages/Details';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<MovieDetail />} />
-        <Route path="/shows" element={<ShowDetail />} />
+        <Route path="/details/:id" element={<Details />} />
       </Routes>
     </>
   );
