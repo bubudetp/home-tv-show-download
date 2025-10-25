05915f8a8f30fece328b03d5c7957cc98ff7133f
Burak Kose <pro.burak.kose@gmail.com>
2025-10-25 15:59:41 +0200

    Add result to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Details from './pages/Details';
+import Results from './pages/Results';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
         <Route path="/details/:id" element={<Details />} />
       </Routes>
     </>
